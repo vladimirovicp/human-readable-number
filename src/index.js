6bc4772a75de6833c417375e2d0c826b218e7a05
vladimirovicp <vladimirovicp70@gmail.com>
2022-06-22 11:36:17 +0400

    add: final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number == 0) { return 'zero'; }
+    let rezult = '';
+    let units = [
+        '',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+    ];
+
+    let dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let largnumbers = ['', ' thousand', ' million', ' billion', ' trillion', ' quadrillion', ' quintillion'];
+
+    let strNumber = number.toString();
+    let masNumber = [];
+    let i = 0;
+
+    while (strNumber.length > 3) {
+        masNumber[i] = strNumber.slice(-3);
+        i++;
+        strNumber = strNumber.slice(0, -3);
+    }
+    masNumber[i] = strNumber;
+    for (let j = masNumber.length - 1; j >= 0; j--) {
+
+        if (masNumber[j][0] == 0) {
+            masNumber[j] = masNumber[j].slice(1);
+        }
+
+        if (masNumber[j] >= 100) {
+            rezult = rezult + ' ' + units[masNumber[j].slice(0, 1)] + ' hundred';
+            masNumber[j] = masNumber[j].slice(1);
+            if (masNumber[j][0] == 0) {
+                masNumber[j] = masNumber[j].slice(1);
+            }
+        }
+        if (masNumber[j] < 100) {
+
+            if (masNumber[j] < 20) {
+                if (masNumber[j] > 0) {
+                    rezult = rezult + ' ' + units[masNumber[j]];
+                }
+
+            } else {
+                rezult = rezult + ' ' + dozens[masNumber[j][0]] + ' ' + units[masNumber[j][1]];
+            }
+        };
+        rezult = rezult + largnumbers[j];
+    }
+    return rezult.trim();
 }
